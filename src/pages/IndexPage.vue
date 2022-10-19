21b8886dd3578a974ea82c2a6ce6305e0e51af15
LoggeL <hyper.xjo@gmail.com>
2022-10-19 02:26:17 +0200

    Add note to front page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,6 +2,7 @@
   <div class="row">
     <div class="col-md-10 col-sm-12 offset-md-1">
       <h2>Welcome to the LRF Tools Page</h2>
+      <h4>Please select a tool on the sidebar</h4>
     </div>
   </div>
 </template>
